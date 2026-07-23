d5213b37378b813f487e2a30d41b04bf2dd4bd8f
André Sousa <andre.magalhaes.sousa@gmail.com>
2026-07-23 12:49:56 +0200

    Generate and display pieces
    Add logic to generate the Connect Four pieces on the grid, and display them merely by spawning a coloured rectangle behind the BoardFront sprite on the appropriate cell.

--- board.gd
+++ board.gd
@@ -1,16 +1,27 @@
 extends Node2D
 
 @export var column_scene: PackedScene
+var board = []
+var next_empty_row = []
 
+enum Piece {
+	EMPTY,
+	RED,
+	YELLOW
+}
+
+const ROW_COUNT := 6
 const COLUMN_COUNT := 7
-const COLUMN_WIDTH := 80
+const CELL_WIDTH := 80
 
 func _ready() -> void:
+	initialize_board()
+	
 	for i in COLUMN_COUNT:
 		var column = column_scene.instantiate()
 		
 		column.column_index = i
-		column.position = Vector2(i * COLUMN_WIDTH, 0)
+		column.position = Vector2(i * CELL_WIDTH, 0)
 		
 		column.column_clicked.connect(_on_column_clicked)
 		
@@ -18,3 +29,26 @@ func _ready() -> void:
 
 func _on_column_clicked(column_index: int) -> void:
 	print("Column ", column_index, " was clicked!")
+	if next_empty_row[column_index] < 0:
+		print("Column ", column_index, " is already full.")
+	else:
+		var new_piece_row : int = next_empty_row[column_index]
+		print("Placing piece on column ", column_index, ", row ", new_piece_row)
+		
+		next_empty_row[column_index] -= 1
+		board[new_piece_row][column_index] = Piece.YELLOW
+		
+		$PieceContainer.spawn_piece(get_cell_position(new_piece_row, column_index))
+
+func initialize_board() -> void:
+	board.clear()
+	next_empty_row.clear()
+	
+	for row in ROW_COUNT:
+		board.append([])
+		for column in COLUMN_COUNT:
+			board[-1].append(Piece.EMPTY)
+			next_empty_row.append(ROW_COUNT - 1)
+
+func get_cell_position(row: int, column: int) -> Vector2:
+	return Vector2(column * CELL_WIDTH, row * CELL_WIDTH)
