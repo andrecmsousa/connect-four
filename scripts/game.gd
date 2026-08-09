821c9ba7e20ef46645100a51ba3e34dce356bd79
André Sousa <andre.magalhaes.sousa@gmail.com>
2026-08-09 18:03:42 +0200

    Make pause menu fully functional
    Ensure the "Start New Game" and "Exit" buttons of the pause menu are fully functional as well.

--- scripts/game.gd
+++ scripts/game.gd
@@ -10,8 +10,17 @@ func resume_game() -> void:
 	pause_menu.hide()
 	get_tree().paused = false
 
+func start_new_game() -> void:
+	get_tree().paused = false
+	get_tree().reload_current_scene()
+
+func exit_game() -> void:
+	get_tree().quit()
+
 func _ready() -> void:
 	pause_menu.resume_pressed.connect(resume_game)
+	pause_menu.new_game_pressed.connect(start_new_game)
+	pause_menu.exit_pressed.connect(exit_game)
 	pause_menu.hide()
 
 func _unhandled_input(event: InputEvent) -> void:
